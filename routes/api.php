b367f60605e3c4705e033435fa7aeb1af4b7e0a9
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-05-30 19:55:31 +0400

    style: email blades

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
 
-Route::post('/register', [RegisterController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/reset-password', [ResetPasswordController::class, 'postPassword']);
+Route::post('/register', [RegisterController::class, 'register'])->name('user.register');
+
+Route::post('/login', [AuthController::class, 'login'])->name('user.login');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('/reset-password', [ResetPasswordController::class, 'postPassword'])->name('password.mail');
+Route::patch('/reset-password/{token}', [ResetPasswordController::class, 'update'])->name('password.update');
+
 Route::post('/email/verify/{token}', [RegisterController::class, 'verifyAccount'])->name('user.verify');
-Route::patch('/reset-password/{token}', [ResetPasswordController::class, 'update'])->name('user.password');
