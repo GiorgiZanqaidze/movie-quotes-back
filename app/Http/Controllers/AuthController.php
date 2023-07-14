24fc715dfd8785cdcacd6029b579d2f00cdb0c6b
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-07-14 10:41:37 +0400

    refactor: remove unnecessary code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,8 +35,6 @@ class AuthController extends Controller
 	{
 		$request->session()->invalidate();
 
-		$request->session()->regenerateToken();
-
 		return response()->json('Successfully logged out');
 	}
 }
